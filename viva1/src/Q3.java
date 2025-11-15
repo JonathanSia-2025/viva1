6f92fdb4e821bb210388319ba600cbc7cb60bce3
Jonathan-2025 <25006171@siswa.um.edu.my>
2025-11-15 23:40:51 +0800

    remove from package

--- viva1/src/Q3.java
+++ viva1/src/Q3.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package viva1;
 
 import java.util.Scanner;
 
@@ -10,7 +9,7 @@ import java.util.Scanner;
  *
  * @author MelodyCloud
  */
-public class Viva1 {
+public class Q3 {
 
     /**
      * @param args the command line arguments
